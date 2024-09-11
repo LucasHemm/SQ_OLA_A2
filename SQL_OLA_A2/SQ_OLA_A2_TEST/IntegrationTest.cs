ed0c346e9a5c01c81c19066b72992e41a741d03d
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2024-09-11 13:54:40 +0200

    Initial commit for test project

---
+++ SQL_OLA_A2/SQ_OLA_A2_TEST/IntegrationTest.cs
@@ -0,0 +1,58 @@
+﻿using SQL_OLA_A2;
+using Task = SQL_OLA_A2.Task;
+
+
+namespace SQ_OLA_A2_TEST;
+using Microsoft.EntityFrameworkCore;
+
+public class IntegrationTests
+{
+    
+    //Mocking the database
+    
+    public IntegrationTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        var context = new ApplicationDbContext(options);
+        taskMapper = new TaskMapper(context);
+        taskFacade = new TaskFacade(context);
+    }
+    
+    private TaskMapper taskMapper;
+    private TaskFacade taskFacade;
+    
+    [Fact]
+    public void ShouldCreateAndPersistTask()
+    {
+        //Test CreateTask
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        taskMapper.AddTask(task);
+        Task taskFromDb = taskMapper.GetTaskById(task.Id);
+        Assert.Equal("Test", taskFromDb.Description);
+        Assert.Equal(now, taskFromDb.Deadline);
+        Assert.False(taskFromDb.IsFinished);
+        Assert.Equal("Test", taskFromDb.Category);
+    }
+    
+    [Fact]
+    public void ShouldUpdateAndPersistTask()
+    {
+        //Test UpdateTask
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        taskMapper.AddTask(task);
+        Task taskFromDb = taskMapper.GetTaskById(task.Id);
+        task = taskFacade.UpdateTask(taskFromDb, "Test2", now, true, "Test2");
+        taskMapper.UpdateTask(task);
+        Task updatedTask = taskMapper.GetTaskById(task.Id);
+        Assert.Equal("Test2", updatedTask.Description);
+        Assert.Equal(now, updatedTask.Deadline);
+        Assert.True(updatedTask.IsFinished);
+        Assert.Equal("Test2", updatedTask.Category);
+    }
+    
+} 
---
+++ SQL_OLA_A2/SQ_OLA_A2_TEST/UnitTests.cs
@@ -0,0 +1,89 @@
+﻿using SQL_OLA_A2;
+using Task = SQL_OLA_A2.Task;
+
+namespace SQ_OLA_A2_TEST;
+using Microsoft.EntityFrameworkCore;
+
+public class UnitTests
+{
+    
+    private TaskFacade taskFacade;
+
+    public UnitTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        var context = new ApplicationDbContext(options);
+        taskFacade = new TaskFacade(context);
+    }
+    
+    [Fact]
+    public void ShouldCreateTask()
+    {
+        //Test CreateTask
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        Assert.Equal("Test", task.Description);
+        Assert.Equal(now, task.Deadline);
+        Assert.False(task.IsFinished);
+        Assert.Equal("Test", task.Category);
+    }
+
+    [Fact]
+    public void ShouldUpdateTask()
+    {
+        // Test UpdateTask
+        DateTime now = DateTime.Now;
+        // Create a FakeTask instance
+        FakeTask task = new FakeTask("Test", now, false, "Test");
+    
+        task = (FakeTask) taskFacade.UpdateTask(task, "Test2", now, true, "Test2");
+    
+        Assert.Equal("Test2", task.Description);
+        Assert.Equal(now, task.Deadline);
+        Assert.True(task.IsFinished);
+        Assert.Equal("Test2", task.Category);
+    }
+
+
+    [Fact]
+    public void ShouldMarkTaskAsFinished()
+    {
+        //Test MarkAsFinished
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        task = taskFacade.MarkAsFinished(task);
+        Assert.True(task.IsFinished);
+    }
+
+    [Fact]
+    public void ShouldMarkTaskAsUnfinished()
+    {
+        //Test MarkAsUnfinished
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, true, "Test");
+        task = taskFacade.MarkAsUnfinished(task);
+        Assert.False(task.IsFinished);
+    }
+
+    
+    private class FakeTask : Task
+    {
+        public FakeTask(string description, DateTime deadline, bool isCompleted, string category) 
+            : base(description, deadline, isCompleted, category)
+        {
+        }
+    }
+
+    [Fact]
+    public void ShouldUpdateDeadline()
+    {
+        DateTime now = DateTime.Now;
+        Task fakeTask = new FakeTask("DummyTask", now, false, "Dummy task description");
+        DateTime newDate = now.AddDays(1);
+        fakeTask = taskFacade.UpdateDeadline(fakeTask, newDate);
+        Assert.Equal(newDate, fakeTask.Deadline);
+    }
+}
