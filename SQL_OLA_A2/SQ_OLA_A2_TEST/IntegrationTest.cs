b0dd125135934e7106dab4175d31244dfbf05620
Matias Rottbøll <mrottboell@gmail.com>
2024-09-11 14:05:38 +0200

    Update IntegrationTest.cs

--- SQL_OLA_A2/SQ_OLA_A2_TEST/IntegrationTest.cs
+++ SQL_OLA_A2/SQ_OLA_A2_TEST/IntegrationTest.cs
@@ -19,10 +19,14 @@ public class IntegrationTests
         var context = new ApplicationDbContext(options);
         taskMapper = new TaskMapper(context);
         taskFacade = new TaskFacade(context);
+        //Clear the database before each test
+        context.Database.EnsureDeleted();
     }
     
     private TaskMapper taskMapper;
     private TaskFacade taskFacade;
+    
+    
     
     [Fact]
     public void ShouldCreateAndPersistTask()
@@ -55,4 +59,62 @@ public class IntegrationTests
         Assert.Equal("Test2", updatedTask.Category);
     }
     
-} 
+    [Fact]
+    public void ShouldGetTaskById()
+    {
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        taskFacade.SaveTask(task);
+        Task taskById = taskFacade.GetTaskById(task.Id);
+        Assert.Equal(task, taskById);
+    }
+    
+    [Fact]
+    public void ShouldDeleteTask()
+    {
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        taskFacade.SaveTask(task);
+        taskFacade.DeleteTask(task);
+        Task taskById = taskFacade.GetTaskById(task.Id);
+        Assert.Null(taskById);
+    }
+    
+    [Fact]
+    public void ShouldGetAllTasks()
+    {
+        DateTime now = DateTime.Now;
+        Task task1 = taskFacade.CreateTask("Test1", now, false, "Test");
+        taskFacade.SaveTask(task1);
+        Task task2 = taskFacade.CreateTask("Test2", now, false, "Test");
+        taskFacade.SaveTask(task2);
+        Task task3 = taskFacade.CreateTask("Test3", now, false, "Test");
+        taskFacade.SaveTask(task3);
+        List<Task> tasks = taskFacade.GetAllTasks();
+        Assert.Equal(3, tasks.Count);
+    }
+    
+    [Fact]
+    public void ShouldSaveTask()
+    {
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        taskFacade.SaveTask(task);
+        Task taskFromDb = taskMapper.GetTaskById(task.Id);
+        Assert.Equal("Test", taskFromDb.Description);
+        Assert.Equal(now, taskFromDb.Deadline);
+        Assert.False(taskFromDb.IsFinished);
+        Assert.Equal("Test", taskFromDb.Category);
+    }
+    
+    [Fact]
+    public void ShouldAddTask()
+    {
+        DateTime now = DateTime.Now;
+        int count = taskFacade.GetAllTasks().Count;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        taskFacade.AddTask("Test", now, false, "Test");
+        int newCount = taskFacade.GetAllTasks().Count;
+        Assert.Equal(count + 1 , newCount);
+    }
+}
